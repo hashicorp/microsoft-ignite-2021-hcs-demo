2f530f4f9079f453ce9bb6ead34018707cbd5bf9
DerekStrickland <1111455+DerekStrickland@users.noreply.github.com>
2021-02-10 17:10:05 -0500

    changes

--- infrastructure/dc1/dc1.tf
+++ infrastructure/dc1/dc1.tf
@@ -14,7 +14,7 @@ resource "azurerm_resource_group" "rg" {
 resource "hcs_cluster" "dc1" {
   resource_group_name      = azurerm_resource_group.us_west_1.name
   managed_application_name = "dc1"
-  email                    = "dstrickland@hashicorp.com"
+  email                    = "education@hashicorp.com"
   cluster_mode             = "production"
   min_consul_version       = "v1.9.1"
   vnet_cidr                = "172.25.16.0/24"
